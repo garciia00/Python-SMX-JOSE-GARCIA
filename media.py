c703ffea2c95806e6b496afe84b6bf89e02a6b99
Jose Garcia Martinez <138895512+garciia00@users.noreply.github.com>
2025-09-19 08:56:44 +0200

    Add files via upload

---
+++ media.py
@@ -0,0 +1,10 @@
+print("Dime 4 numeros enteros positivos: ")
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+
+total = n1 + n2 + n3 + n4
+media = total / 4
+print(f"La suma es: {total} y la media es: {media:.3f}")
---
+++ saludo.py
@@ -0,0 +1,6 @@
+print("Hola, ¿cómo te llamas?")
+nombre = input()
+print("¿Y cuantos años tienes?")
+años = int(input())
+
+print(f"Hola {nombre}, veo que tienes {años} años. Disfruta del curso de Python!")
